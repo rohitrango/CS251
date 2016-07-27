e9017d9c48896588b20aaeb4e990bc628f25a80b
Akash Trehan <akash.trehan123@gmail.com>
2016-07-27 18:19:16 +0530

    Added final submission folder

--- Lab1/final_state_3.m
+++
@@ -1,9 +0,0 @@
-function y = final_state_3(initial, action)			%performs action on initial matrix and returns the answer
-A = [1 1 0 1 0 0 0 0 0; 1 1 1 0 1 0 0 0 0; 0 1 1 0 0 1 0 0 0; 1 0 0 1 1 0 1 0 0; 0 1 0 1 1 1 0 1 0; 0 0 1 0 1 1 0 0 1; 0 0 0 1 0 0 1 1 0; 0 0 0 0 1 0 1 1 1; 0 0 0 0 0 1 0 1 1 ];
-% A is the 9*9 matrix where A(i,j) = 1 <==> button i is toggled on pressing button j
-b = initial'(:);		
-x = action'(:);
-% b and x are the column vectors of initial and action, for easier calculation
-z = mod((b + A*x),2);		% algorithm gives the required solution
-y = reshape(z,3,3)';		% reshape to 3*3 matrix
-endfunction
