a1ea438e0c7780af508c21296261aed4814d6b30
Akash Trehan <akash.trehan123@gmail.com>
2016-07-30 20:24:31 +0530

    Lab1 submitted

--- Lab1/Outlab Final Submission/main_task_A2.m
+++ Lab1/Outlab Final Submission/main_task_A2.m
@@ -1,7 +1,20 @@
-warning('off');
-i = 10;
-info = -1;
-while(info!=1)
-[x2,residue,info] = fsolve(@ythtr,40)
-i += 10;
-endwhile;
+for i = [1:11:90]
+[shootangle,residue,info] = fsolve('ytheta',40);			%solves for shootangle with different initial guesses
+if(info==1)												%implies correct solution found
+break;
+endif;
+endfor;
+
+[xtie ytie xwing ywing v2 vlaser d w n t1] = num2cell(load('input_outlab_task_A2.txt')){:};		%reads from file and assigns the variables respectively
+
+x = xwing-xtie;
+y = ywing-ytie;											%relative position required
+r = asind(sind(shootangle)/n); 							%snells law
+t = (w*tand(r)*(n*n-1)+x)/(vlaser*sind(shootangle));	%time between shoot and hit = sum of (delta x)/(x comp of velocity)
+
+final_xpos = xwing;										%final position of xwing
+final_ypos = ywing+v2*(t1+t);
+
+fid=fopen('output_outlab_task_A2.txt','w+');
+fprintf(fid,"%.2f %f %f",shootangle,xwing,ywing+v2*(t1+t));
+fclose(fid);
