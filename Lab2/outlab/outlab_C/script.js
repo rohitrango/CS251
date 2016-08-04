1e6a48f02832afa7852984c25278100ff766bb73
rohitrango <rohit.rango@gmail.com>
2016-08-04 19:49:41 +0530

    animation included in task C

--- Lab2/outlab/outlab_C/script.js
+++ Lab2/outlab/outlab_C/script.js
@@ -1,6 +1,51 @@
 var elements = document.getElementsByClassName('panel-title');
 var length = elements.length;
 
+// for(var i=0;i<length;i++) {
+// 	elements[i].addEventListener('click',function() {
+// 		// console.log(this);
+// 		var panel = this.parentNode;					//panel corresponding to this title
+// 		var content = this.nextElementSibling;			//content of the title
+// 		var accordion = panel.parentNode;				//accordion
+// 		// console.log(accordion);
+// 		var el = accordion.getElementsByClassName("panel-body"); var l = el.length;		//all panels of this accordion
+// 		console.log("Length of current accordion - "+String(l));		
+// 		var swap = 0;
+// 		for(var j=0;j<l;j++) {
+// 			// console.log(el[j].style.display);
+// 			if(el[j].style.display !== "none" && el[j]!=content) {
+// 				swap = el[j];
+// 				break;
+// 			}
+// 			else if(el[j].style.display !== "none" && el[j]==content) {
+// 				swap = el[j];
+// 			}
+// 		}
+// 		if(swap===0) {							//no tab is open
+// 			console.log("No other element to close.");
+// 			content.style.display = "block";
+// 		}
+// 		else if(swap!=content) {	
+// 			console.log(swap);									//log the panel to close
+// 			swap.style.display = "none";		
+// 			content.style.display = "block";
+// 		}
+// 		else if(swap==content) {
+// 			content.style.display = "none";
+// 		}
+		
+		
+// 	});
+// }
+function isNamePresent(value,classlist) {
+	var length = classlist.length;
+	for(var i=0;i<length;i++) {
+		if(value===classlist[i])
+			return true;
+	}
+	return false;
+}
+
 for(var i=0;i<length;i++) {
 	elements[i].addEventListener('click',function() {
 		// console.log(this);
@@ -13,25 +58,25 @@ for(var i=0;i<length;i++) {
 		var swap = 0;
 		for(var j=0;j<l;j++) {
 			// console.log(el[j].style.display);
-			if(el[j].style.display !== "none" && el[j]!=content) {
+			if(isNamePresent('show',el[j].classList) && el[j]!=content) {			// some other panel is open
 				swap = el[j];
 				break;
 			}
-			else if(el[j].style.display !== "none" && el[j]==content) {
+			else if(isNamePresent('show',el[j].classList) && el[j]==content) {				//the same panel is open, save and skip it
 				swap = el[j];
 			}
 		}
 		if(swap===0) {							//no tab is open
 			console.log("No other element to close.");
-			content.style.display = "block";
+			content.classList.toggle('show');
 		}
 		else if(swap!=content) {	
 			console.log(swap);									//log the panel to close
-			swap.style.display = "none";		
-			content.style.display = "block";
+			swap.classList.toggle("show");
+			content.classList.toggle("show");
 		}
 		else if(swap==content) {
-			content.style.display = "none";
+			content.classList.toggle("show");
 		}
 		
 		
