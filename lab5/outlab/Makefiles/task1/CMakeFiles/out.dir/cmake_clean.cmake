6bf17244b9aee94ad21a542e53b02477e8dbe0a5
Akash Trehan <akash.trehan123@gmail.com>
2016-09-03 11:38:11 +0530

    Added taskA to submission

---
+++ lab5/outlab/Makefiles/task1/CMakeFiles/out.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/out.dir/Source_Files/dependency1.cpp.o"
+  "CMakeFiles/out.dir/Source_Files/dependency2.cpp.o"
+  "CMakeFiles/out.dir/Source_Files/main.cpp.o"
+  "out.pdb"
+  "out"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
