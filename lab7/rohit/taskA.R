568ed749c431cb93640dce9361e3750dc3c123d9
rohitrango <rohit.rango@gmail.com>
2016-09-22 02:31:57 +0530

    Done i guess

--- lab7/rohit/taskA.R
+++ lab7/rohit/taskA.R
@@ -17,8 +17,26 @@ for(i in 1:samples-1) {
     index = i
   }
 }
-abline(lm(y[1:index] ~ x[1:index]),col="blue")
-abline(lm(y[index+1:samples] ~ x[index+1:samples]),col="green")
+fit1 = lm(y[1:index] ~ x[1:index])
+fit2 = lm(y[index+1:samples] ~ x[index+1:samples])
+m1 = fit1$coefficients[2] ; c1 = fit1$coefficients[1];
+m2 = fit2$coefficients[2] ; c2 = fit2$coefficients[1];
+x_0 = (c1 - c2)/(m2-m1)
+
+t = 1
+while(x[t+1] < x_0) {
+  segments(x[t],m1*x[t]+c1,x1 = x[t+1], m1*x[t+1] + c1, col="darkblue")
+  t = t+1
+}
+segments(x[t],m1*x[t]+c1,x1 = x_0, m1*x_0 + c1, col="blue")
+segments(x_0,m2*x_0+c2,x1 = x[t+1], m2*x[t+1] + c2, col="blue")
+t = t+1
+while(t < samples-1) {
+  segments(x[t],m2*x[t]+c2,x1 = x[t+1], y1 = m2*x[t+1] + c2, col="darkblue")
+  t = t+1
+}
+
 cat('error lm =',sum(simple_lm$residuals^2),"\n")
 cat('error broken =',minError,"\n")
 cat('C =',x[index],"\n")
+  
