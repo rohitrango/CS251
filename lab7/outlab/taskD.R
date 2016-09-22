66ae2ab7be783dec1a4677a66e421e792f88a7bd
Meet <meet11061997@gmail.com>
2016-09-22 23:39:55 +0530

    clearup

--- lab7/outlab/taskD.R
+++ lab7/outlab/taskD.R
@@ -1,12 +1,26 @@
-mfpdata = read.csv("mutualFundPerformance.csv")
+rawdata = as.list(read.csv("mutualFundPerformance.csv"))
+
+#loads rawdata into usable format udata{
+
+#converts each cell to list{
 lister = function(x){
   if(class(x)=="numeric" || class(x)=="logical") return(x)
-  sx = strsplit(x,'\"')[[1]]
-  newx = sx[length(sx)]
-  x.split = strsplit(newx,' ')
-  return(lapply(x.split, as.numeric))[[1]]
+  if(x=="") return(NA)
+  stripped = strsplit(x,'\"')[[1]]
+  charlist = strsplit(stripped[length(stripped)],' ')
+  return(lapply(charlist, as.numeric))[[1]]
 }
-columnstandardize = function(col){
-  f = as.list(col)[[1]]
-  op  = lapply(levels(f),lister)
+#}
+colproc = function(col){
+  if(class(col)=="factor"){
+    lolol = (lapply(col,function(x) lister(levels(factor(x))))) #listoflistoflist
+    return(lapply(lolol,function(x) x[1][[1]]))
+  }
+  else{
+    lolol = (lapply(col,list))
+    return(lapply(lolol,function(x) x[1][[1]]))
+  }
 }
+udata = lapply((2:length(rawdata)),function(x) colproc(rawdata[[x]]))
+
+#}
