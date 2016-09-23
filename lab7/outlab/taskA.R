7ed3bc501dfdac0575d0726b8c82ab731a5dae2a
rohitrango <rohit.rango@gmail.com>
2016-09-23 16:45:06 +0530

    TaskA,B done

--- lab7/outlab/taskA.R
+++ lab7/outlab/taskA.R
@@ -1,25 +1,39 @@
+brokenReg = function(x,y) {
+  index = 0
+  b0 = b1 = b2 = 1
+  minError = Inf
+  n = length(x)
+  
+  for(i in 1:n) {
+    x1 = x;
+    x2 = (x >= x[i])*(x-x[i])
+    minFit = lm(y ~ x1 + x2)
+    error = sum(minFit$residuals^2)
+    if(error < minError) {
+      minError = error
+      index = i
+      b0 = minFit$coefficients[1]
+      b1 = minFit$coefficients[2]
+      b2 = minFit$coefficients[3]
+    }
+  }
+  
+  x2 = (x >= x[index])*(x-x[index])
+  plot(x,y)
+  lines(x,b0 + b1*x + b2*x2, col="blue")
+  return(c(index,minError,b0,b1,b2))
+}
+
 taskAdata = read.csv('data_bi_lin.csv')
-taskAdata = taskAdata[with(taskAdata, order(x),),]
+taskAdata = taskAdata[with(taskAdata, order(x)),]
 x = taskAdata$x
 y = taskAdata$y
 samples = length(x)
 simple_lm = lm(y ~ x)
-plot(x,y)
-abline(simple_lm , col= "red")
 
-index = 0
-minError = Inf
-for(i in 1:samples-1) {
-  e1 = sum(lm(y[1:i] ~ x[1:i])$residuals^2)
-  e2 = sum(lm(y[i+1:samples] ~ x[i+1:samples])$residuals^2)
-  if(e1 + e2 < minError) {
-    minError = e1+e2
-    index = i
-  }
-}
-
-abline(lm(y[1:index] ~ x[1:index]),col="blue")
-abline(lm(y[index+1:samples] ~ x[index+1:samples]),col="green")
+regressionResults = brokenReg(x,y)
+abline(simple_lm,col="red")
 cat('error lm =',sum(simple_lm$residuals^2),"\n")
-cat('error broken =',minError,"\n")
-cat('C =',x[index],"\n")
+cat('error broken =',regressionResults[2],"\n")
+cat('C =',x[regressionResults[1]],"\n")
+
