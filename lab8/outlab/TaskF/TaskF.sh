396b5151745e109dbc1a3ae7858958e4400e4f0f
meettaraviya <meettaraviya@github.com>
2016-09-27 22:25:54 +0530

    taskF changes

--- lab8/outlab/TaskF/TaskF.sh
+++ lab8/outlab/TaskF/TaskF.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
+## TAKING USER+SYS TIME AND PRINTING ORDER
 for f in ` find | grep '[0-9]' | sed -r 's/\.\/([0-9]*)/\1/'`; do
-	x=$({ time -p ./$f -l >/dev/null; } 2>&1 | grep real | tr -d a-z[:space:])
-	arr+=("$x $f")
+	times=$({ time -p ./$f -l >/dev/null; } 2>&1)
+	utime=$(echo $times | cut -d' ' -f4 | sed -r 's/\.//')
+	stime=$(echo $times | cut -d' ' -f6 | sed -r 's/\.//')
+	total=$(echo $utime + $stime | bc)
+	arr+=("$total $f")
 done
-printf '%s\n' "${arr[@]}" | sort | cut -c6-7
+printf '%s\n' "${arr[@]}" | sed -r 's/(^[0-9]{2} )/0\1/' | sort | cut -d' ' -f2
+
+for f in `printf '%s\n' "${arr[@]}" | sed -r 's/(^[0-9]{2} )/0\1/' | sort | cut -d' ' -f2`; do
+	./$f
+done
+
+## TAKING REAL TIME AND PRINTING ORDER
+# for f in ` find | grep '[0-9]' | sed -r 's/\.\/([0-9]*)/\1/'`; do
+# 	times=$({ time -p ./$f -l >/dev/null; } 2>&1)
+# 	rtime=$(echo $times | cut -d' ' -f2 | sed -r 's/\.//')
+# 	arr+=("$rtime $f")
+# done
+# printf '%s\n' "${arr[@]}" | sed -r 's/(^[0-9]{2} )/0\1/' | sort | cut -d' ' -f2
+
+# for f in `printf '%s\n' "${arr[@]}" | sed -r 's/(^[0-9]{2} )/0\1/' | sort | cut -d' ' -f2`; do
+# 	./$f
+# done
+
+
+## IF BACKGROUND PROCESSES ALLOWED AND WE DONT HAVE TO PRINT ORDER
+# for f in ` find | grep '[0-9]' | sed -r 's/\.\/([0-9]*)/\1/'`; do
+# 	./$f &
+# done
