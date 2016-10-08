612e71096b24dafc0a4e4d144dd8a8a4304b3e80
Meet <meet11061997@gmail.com>
2016-10-08 16:43:49 +0530

    fixed B

--- lab9/resources/support/taskB/Map.java
+++ lab9/resources/support/taskB/Map.java
@@ -8,34 +8,34 @@ class Map{
     	
       	Integer m=Integer.parseInt(sc.next()),
       		n=Integer.parseInt(sc.next()),
-      		s=Integer.parseInt(sc.next()),
-      		p=Integer.parseInt(sc.next());
+      		s=Integer.parseInt(sc.next())-1,
+      		p=Integer.parseInt(sc.next())-1;
 
       	Boolean vstd[] = new Boolean[m];
-      	Integer mindist[] = new Integer[m];
-     	Integer mtx[][] = new Integer[m][m];
+      	Double mindist[] = new Double[m];
+     	Double mtx[][] = new Double[m][m];
      	for(int i=0; i<m; i++){
      		vstd[i]=false;
-     		mtx[i][i]=0;
-     		mindist[i]=Integer.MAX_VALUE;
+     		mtx[i][i]=0.0;
+     		mindist[i]=Double.POSITIVE_INFINITY;
      		for(int j=i+1; j<m; j++){
-     			mtx[i][j]=Integer.MAX_VALUE;
-     			mtx[j][i]=Integer.MAX_VALUE;
+     			mtx[i][j]=Double.POSITIVE_INFINITY;
+     			mtx[j][i]=Double.POSITIVE_INFINITY;
      		}
      	}
       	for(int k=0; k<n; k++){
-      	Integer vi=Integer.parseInt(sc.next()),
-      		vj=Integer.parseInt(sc.next()),
-      		wt=Integer.parseInt(sc.next());
+      	Integer vi=Integer.parseInt(sc.next())-1,
+      		vj=Integer.parseInt(sc.next())-1;
+      	Double wt= (double) Integer.parseInt(sc.next());
       		mtx[vi][vj]=wt;
       		mtx[vj][vi]=wt;
 
       	}
-      	mindist[s]=0;
+      	mindist[s]=0.0;
       	int c=s;
       	while(c!=p){
       		int nextc=-1; 
-      		Integer nextcmindist=Integer.MAX_VALUE;
+      		Double nextcmindist=Double.POSITIVE_INFINITY;
       		for(int i=0; i<m; i++){
       			if(!vstd[i]){
       				if(mindist[c]+mtx[c][i]<mindist[i]) mindist[i] =mindist[c]+mtx[c][i];
@@ -46,12 +46,16 @@ class Map{
       			}
       		}
       		vstd[c] = true;
-      		c = nextc;
-      		nextcmindist = Integer.MAX_VALUE;
+      		if(nextc>=0) c = nextc;
+          else{
+            c=0;
+            while(c!=p && vstd[c]) c++;
+          }
+      		nextcmindist = Double.POSITIVE_INFINITY;
 
       	}
-      	if(mindist[p]==Integer.MAX_VALUE) System.out.println("Path does not exist\n");
-      	else System.out.println("Length of shortest path = "+mindist[p].toString());
+      	if(mindist[p].isInfinite()) System.out.println("Path does not exist\n");
+      	else System.out.println("Length of shortest path = "+((Integer)(int)(double)mindist[p]).toString());
 
       
    }
