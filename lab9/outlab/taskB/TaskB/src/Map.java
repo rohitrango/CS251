2450bac180c9279f1470e76ab10a914b5d69e74d
Akash Trehan <akash.trehan123@gmail.com>
2016-10-09 06:08:57 +0530

    Found bug in dijkstra's implementation

--- lab9/outlab/taskB/TaskB/src/Map.java
+++ lab9/outlab/taskB/TaskB/src/Map.java
@@ -11,12 +11,15 @@ public class Map{
       		s=Integer.parseInt(sc.next())-1,
       		p=Integer.parseInt(sc.next())-1;
 
-      	Boolean vstd[] = new Boolean[m];
-      	Integer mindist[] = new Integer[m];
-     	Integer mtx[][] = new Integer[m][m];
+      	Boolean vstd[] = new Boolean[m]; //Visited or not
+      	Integer mindist[] = new Integer[m]; // Minimum Distance
+     	Integer mtx[][] = new Integer[m][m]; // Matrix to store neighbors
+     	Integer path[] = new Integer[m];
+     	
      	for(int i=0; i<m; i++){
      		vstd[i]=false;
      		mtx[i][i]=0;
+     		path[i] = -1;
      		mindist[i]=2147483647; // Maximum value for Integer
      		for(int j=i+1; j<m; j++){
      			mtx[i][j]=2147483647;
@@ -31,31 +34,56 @@ public class Map{
       		mtx[vj][vi]=wt;
 
       	}
+      	
+//      	vstd[s] = true;
       	mindist[s]=0;
+      	
       	int c=s;
       	while(c!=p){
       		int nextc=-1;
       		Integer nextcmindist=2147483647;
       		for(int i=0; i<m; i++){
-      			if(!vstd[i]){
-      				if(mindist[c]+mtx[c][i]<mindist[i]) mindist[i] =mindist[c]+mtx[c][i];
-      				if(nextcmindist>mindist[i]){
-      					nextc = i;
-      					nextcmindist = mindist[i];
-      				}
+      			if(!vstd[i] && mtx[c][i] != 2147483647){
+      				if(mindist[c]+mtx[c][i] < mindist[i])
+      					mindist[i] = mindist[c]+mtx[c][i];
+      				
       			}
+      			if(!vstd[i] && nextcmindist > mindist[i] && i != c){
+  					nextc = i;
+  					nextcmindist = mindist[i];
+  				}
       		}
       		vstd[c] = true;
-      		if(nextc>=0) c = nextc;
-          else{
-            c=0;
-            while(c!=p && vstd[c]) c++;
-          }
+      		if(nextc >= 0)
+  			{
+      			path[nextc] = c; // This is not correct.
+  				c = nextc;
+  			}
+            else{
+	            c=0;
+	            while(c!=p && vstd[c]) c++;
+            }
       		nextcmindist = 2147483647;
 
       	}
       	if(mindist[p] == 2147483647) System.out.println("Path does not exist\n");
-      	else System.out.println("Length of shortest path = "+((Integer)(int)mindist[p]).toString());
+      	else
+  		{
+      		System.out.println("Length of shortest path = "+((Integer)(int)mindist[p]).toString());
+      		System.out.print("Path: ");
+      		Stack pathStack = new Stack();
+      		int k = p;
+      		while(k != s)
+      		{
+      			pathStack.push(k);
+      			k = path[k];
+      		}
+      		
+      		while(!pathStack.isEmpty())
+      		{
+      			System.out.print(Integer.toString(((int)pathStack.pop() + 1)) + " ");
+      		}
+  		}
 
       	sc.close();
    }
